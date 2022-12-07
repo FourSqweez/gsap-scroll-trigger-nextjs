b6f6b9276acf9858ed108225bf2e7e6f0f99abc4
FourSqweez <four7518@gmail.com>
2022-12-07 17:01:59 +0700

    create timeline page using delay

---
+++ pages/timeline.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useRef } from 'react'
+import styles from '../styles/timeline.module.css'
+import gsap from 'gsap'
+import ScrollTrigger from 'gsap/dist/ScrollTrigger'
+
+gsap.registerPlugin(ScrollTrigger)
+
+const TimelinePage = () => {
+  const boxRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    gsap.to(boxRef.current, {
+      x: 500,
+      duration: 2,
+    })
+
+    gsap.to(boxRef.current, {
+      y: 200,
+      duration: 3,
+      delay: 2,
+    })
+
+    gsap.to(boxRef.current, {
+      x: 0,
+      duration: 2,
+      delay: 5,
+    })
+  }, [])
+
+  return <div className={styles.box} ref={boxRef}></div>
+}
+
+export default TimelinePage
